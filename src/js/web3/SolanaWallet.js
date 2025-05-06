2bbdf7176ac96aa8e703c12ec65b7300443c1d32
alexrods <alexrodsanch@gmail.com>
2025-05-06 00:55:40 -0600

    change spl token

--- src/js/web3/SolanaWallet.js
+++ src/js/web3/SolanaWallet.js
@@ -26,7 +26,7 @@ export class SolanaWallet {
     this.walletDropdownVisible = false;
 
     this.Credit_DECIMALS = 6;
-    this.Credit_MINT_ADDRESS = "dsitAJapavcqf2UsXzP4jBz2wBC6nu3od7D45idwiSU";
+    this.Credit_MINT_ADDRESS = "7d7HjBSdPP6jixeUpoQKxoF7vfEUU87XG3mgTCvTbjD1";
     this.Bonk_DECIMALS = 6;
     this.Bonk_MINT_ADDRESS = "dsitAJapavcqf2UsXzP4jBz2wBC6nu3od7D45idwiSU";
     this.ADMIN_WALLET_ADDRESS = "CGacpojgdVcuUBZuFJqWu3eiEVcbXAxQcXXridgvFhRg";
